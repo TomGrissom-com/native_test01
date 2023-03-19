a9249f3b314089b12cc9cd185163788fcde6231f
TomGrissom-com <88014699+TomGrissom-com@users.noreply.github.com>
2023-03-19 15:50:35 -0500

    3/19/23

--- App.js
+++ App.js
@@ -10,13 +10,19 @@ import {
   TextInput,
   BackHandler,
   KeyboardAvoidingView,
-  TouchableOpacity
+  TouchableOpacity,
+  StatusBar,
+  ImageBackground,
+  ToastAndroid,
+  Image,
+  Linking
 } from 'react-native';
 
 
 //Gets screen size and allows for adjustments
 const { width, height } = Dimensions.get('window');
 const fontScale = Math.sqrt(width * height) / 100;
+const NotificationBarHeight = StatusBar.currentHeight
 
 const Flex = () => {
 //general states
@@ -70,14 +76,19 @@ const Flex = () => {
                             price: productPrice,
                             taxed: taxable
                           }
-
-    if(productDesc.trim() !== "" || productPrice.trim() !== ""){
+                          
+    
+    if(productPrice.trim() !== ""){
       setList([...list, itemsToAddToList])
       setTaxable(false)
       setProductDesc('')
       setProductPrice('')
-    }
-    setIdNum(idNum + 1) 
+      setIdNum(idNum + 1) 
+      {!productDesc ? ToastAndroid.show('Not Required But We Recomend Adding A Description', ToastAndroid.LONG):""}                          
+    
+      ToastAndroid.show('Product Added', ToastAndroid.SHORT)
+    
+    }else{ alert("Product Price Required")}
   }
  const handleDeleteItem = (id)=>{
   const filteredItems = list.filter((item) => item.id !== id);
@@ -89,11 +100,12 @@ const Flex = () => {
     setList(updatedArray);
   };
 
-const handleSetBudget = (bl)=>{
-  setBudgetLimit(bl)
-  setChangeBudget(true)
-}
-
+  const handleSetBudget = (bl)=>{
+    ToastAndroid.show('Tap Blue Text To Change Budget', ToastAndroid.SHORT);
+    setBudgetLimit(bl)
+    setChangeBudget(true)
+  }
+  
 //RENDER ITEMS FROM LIST
   const renderItems = ({item, index})=>(
     <View style={styles.listItemRow}>
@@ -119,135 +131,176 @@ const handleSetBudget = (bl)=>{
   )
 
 //css variables
-  const budgetColor = Number(budgetLimit).toFixed(2)-Number(totalPrice).toFixed(2) <= 5 ? "red":"lightgreen"
+  const budgetColor = 
+    Number(budgetLimit).toFixed(2)-Number(totalPrice).toFixed(2) <=10 
+    && 
+    Number(budgetLimit).toFixed(2)-Number(totalPrice).toFixed(2) >1
+    ? 
+    "rgb(247,128,74)"
+    : 
+    Number(budgetLimit).toFixed(2)-Number(totalPrice).toFixed(2) <= 1 ? "red":"green"
 
+//VIEW PORT    
   return (
-    <View
-      style={[
-        styles.container,
-        {
-          flexDirection: 'column',
-          minHeight: Math.round(Dimensions.get('window').height)
-        },
-      ]}>
-      <View style={[styles.subContainer,{flex: 2, borderWidth: 2, justifyContent: 'center'}]}  >
-        <Text style={styles.header}>Budget As You Shop App</Text>
-        <Text style={[styles.header,{fontSize: fontScale*2}]}>By: BetterTech</Text>
-        <View style={[styles.subContainer, {flexDirection:'row'}]}>
-          <View style={[styles.subContainer,{flex: 1}]}>
-            {
-              !changeBudget ? 
-              <View style={[styles.subContainer, {flexDirection:'row'}]}>
-                <View style={[styles.subContainer, {flex:2, justifyContent:"center"}]}>
-                  <TextInput
-                  style={styles.input}
-                  onChangeText={setBudgetLimit}
-                  value={budgetLimit}
-                  placeholder="Set Budget Amount"
-                  keyboardType="numeric"
-                  />
+    <ImageBackground source={require('./assets/splash.png')} style={styles.background}>  
+      <View
+        style={[
+          styles.container,
+          {
+            flexDirection: 'column',
+            minHeight: Math.round(Dimensions.get('window').height)
+          },
+        ]}>
+        <View style={[styles.subContainer,{flex: 2, justifyContent: 'center'}]}  >
+          <Text style={styles.header}>Budget As You Shop</Text>
+          <Text style={[styles.header,{fontSize: fontScale*2}]}>By: BetterTech</Text>
+          <View style={[styles.innerSubContainer, {flexDirection:'row'}]}>
+            <View style={[styles.innerSubContainer,{flex: 1}]}>
+              {
+                !changeBudget ? 
+                <View style={[styles.innerSubContainer, {flexDirection:'row'}]}>
+                  <View style={[styles.innerSubContainer, {flex:2, justifyContent:"center"}]}>
+                    <TextInput
+                    style={styles.input}
+                    onChangeText={setBudgetLimit}
+                    value={budgetLimit}
+                    placeholder="Set Budget Amount"
+                    keyboardType="numeric"
+                    />
+                  </View>
+                  <View style={[styles.innerSubContainer, {flex:1, justifyContent:"center"}]}>
+                    <Button title="Set Budget" onPress={()=>handleSetBudget(budgetLimit)}/>
+                  </View>
                 </View>
-                <View style={[styles.subContainer, {flex:1, justifyContent:"center"}]}>
-                  <Button title="Set Budget" onPress={()=>handleSetBudget(budgetLimit)}/>
-                </View>
+              :
+
+              <View style={[styles.innerSubContainer, {flexDirection:'row'}]}>
+                <TouchableOpacity onPress={()=>setChangeBudget(false)}>
+                  <Text style={[styles.header,{color: "blue",fontSize: fontScale*5,fontWeight: 'bold'}]}>{`Budget: $${Number(budgetLimit).toFixed(2)}`}</Text>
+                </TouchableOpacity>
               </View>
-            :
-            <TouchableOpacity onPress={()=>setChangeBudget(false)}>
-              <Text style={[styles.header,{color: budgetColor,fontSize: fontScale*5,fontWeight: 'bold'}]}>{`Budget: $${Number(budgetLimit-totalPrice).toFixed(2)}`}</Text>
-            </TouchableOpacity>
-            }
+              
+              }
+            </View>
           </View>
         </View>
-      </View>
-      <View style={[styles.subContainer,{flex: 2, borderWidth: 2,}]} >
-        <Text style={styles.header}>{`Total Taxed: $${totalTaxed.toFixed(2)}`}</Text>
-        <Text style={styles.header}>{`Total Nontaxed: $${totalUntaxed.toFixed(2)}`}</Text>
-        <Text style={[styles.header,{fontSize: fontScale*5,fontWeight: 'bold'}]}>{`Total price: $${totalPrice.toFixed(2)}`}</Text>
-      </View>
-      <View style={[styles.subContainer,{flex: 3, borderWidth: 2,}]} >
-        <View style={styles.subContainer}>
-          <TextInput
-          style={styles.input}
-          onChangeText={setProductDesc}
-          value={productDesc}
-          placeholder="Product Description"
-          keyboardType="default"
-          />
+        <View style={[styles.subContainer,{flex: 1.5, backgroundColor:"lightgray"}]} >
+          {
+            budgetLimit ==0 
+            ?
+            <Text style={[styles.header,{fontSize: fontScale*5,fontWeight: 'bold'}]}>Set Your Budget Above</Text>
+            :
+            <>
+              <Text style={[styles.header,{fontSize: fontScale*5,fontWeight: 'bold'}]}>{`Products: $${totalPrice.toFixed(2)}`}</Text>
+              <Text style={[styles.header,{color: budgetColor,fontSize: fontScale*5,fontWeight: 'bold'}]}>{`Remaining: $${Number(budgetLimit-totalPrice).toFixed(2)}`}</Text>
+            </>
+          
+          }
         </View>
-        <View style={[styles.subContainer, {flexDirection:'row'}]}>
-          <View style={[styles.subContainer,{flex: 2}]}>
+        <View style={[styles.subContainer,{flex: 2.5, marginBottom:5,marginTop:5}]} >
+          <View style={styles.innerSubContainer}>
             <TextInput
             style={styles.input}
-            onChangeText={setProductPrice}
-            value={productPrice}
-            placeholder="Product Price"
-            keyboardType="numeric"
+            onChangeText={setProductDesc}
+            value={productDesc}
+            placeholder="Product Description"
+            keyboardType="default"
             />
           </View>
-          <View style={[styles.subContainer,{flex: 1, alignItems:"center"}]}>
-            <Text style={styles.text}>Taxable</Text>
-            <Switch
-              trackColor={{false: '#767577', true: '#81b0ff'}}
-              thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-              ios_backgroundColor="#3e3e3e"
-              onValueChange={()=>setTaxable(previousState => !previousState)}
-              value={taxable}
-            />
-          </View>
-        </View>
-          <Button title="Add item" onPress={handleAddItem} />
-      </View>
-      <View style={[styles.subContainer,{flex: 6, borderWidth: 2,}]} >
-        <View style={[styles.subContainer,{flex: 1, borderWidth: 2,}]} >
-          <View style={styles.listItemRow}>
-            <View style={[styles.itemBox,{flex:2}]}>
-              <Text style={[styles.listItemText, {fontSize: fontScale*3,fontWeight:'bold'}]}>Item</Text>
+          <View style={[styles.innerSubContainer, {flexDirection:'row'}]}>
+            <View style={[styles.innerSubContainer,{flex: 2}]}>
+              <TextInput
+              style={styles.input}
+              onChangeText={setProductPrice}
+              value={productPrice}
+              placeholder="Product Price"
+              keyboardType="numeric"
+              />
             </View>
-            <View style={[styles.itemBox, {flex:2, alignItems: 'center'}]}>
-              <Text style={[styles.listItemText, {fontSize: fontScale*3,fontWeight:'bold'}]}>Price</Text>
-            </View>
-            <View style={[styles.itemBox, {alignItems: 'center'}]}>
-              <Text style={[styles.listItemText, {fontSize: fontScale*3,fontWeight:'bold'}]}>Tax?</Text>
-            </View>
-            <View style={[styles.itemBox, {flex:2, alignItems: 'center'}]}>
-              <Text style={[styles.listItemText, {fontSize: fontScale*3,fontWeight:'bold'}]}>Delete?</Text>
+            <View style={[styles.innerSubContainer,{flex: 1, alignItems:"center"}]}>
+              <Text style={styles.text}>Taxable</Text>
+              <Switch
+                trackColor={{false: '#767577', true: '#81b0ff'}}
+                thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+                ios_backgroundColor="#3e3e3e"
+                onValueChange={()=>setTaxable(previousState => !previousState)}
+                value={taxable}
+              />
             </View>
           </View>
-          <FlatList
-            style={styles.list}
-            data={list}
-            renderItem={renderItems}
-            keyExtractor={(list, index) => index.toString()}/>
+            <Button title="Add item" onPress={handleAddItem} />
+        </View>
+        <View style={[styles.subContainer,{flex: 6}]} >
+          <View style={[styles.innerSubContainer,{flex: 1, marginTop:5, backgroundColor:'lightgray', borderBottomRightRadius:10, borderBottomLeftRadius: 10}]} >
+            <View style={styles.listItemRow}>
+              <View style={[styles.itemBox,{flex:2}]}>
+                <Text style={[styles.listItemText, {fontSize: fontScale*3,fontWeight:'bold'}]}>Item</Text>
+              </View>
+              <View style={[styles.itemBox, {flex:2, alignItems: 'center'}]}>
+                <Text style={[styles.listItemText, {fontSize: fontScale*3,fontWeight:'bold'}]}>Price</Text>
+              </View>
+              <View style={[styles.itemBox, {alignItems: 'center'}]}>
+                <Text style={[styles.listItemText, {fontSize: fontScale*3,fontWeight:'bold'}]}>Tax?</Text>
+              </View>
+              <View style={[styles.itemBox, {flex:2, alignItems: 'center'}]}>
+                <Text style={[styles.listItemText, {fontSize: fontScale*3,fontWeight:'bold'}]}>Delete?</Text>
+              </View>
+            </View>
+            <FlatList
+              style={styles.list}
+              data={list}
+              renderItem={renderItems}
+              keyExtractor={(list, index) => index.toString()}/>
+          </View>
+        </View>
+        <View style={styles.btads}>
+          <TouchableOpacity onPress={()=>{Linking.openURL("https://www.bettertechsol.com/")}}>
+            <Image source={require('./assets/btbanner.png')} style={styles.image} />
+          </TouchableOpacity>
         </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 };
 
-const borderCol = 'blue'
+const borderCol = 'red'
+const borderWidth = 0
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
-    padding: 5,
-    backgroundColor: 'black',
+    resizeMode: 'cover',
+  },
+  container: {
+    marginTop: NotificationBarHeight,
+    flex: 1, 
+    backgroundColor: 'rgba(255, 255, 255, 1)'
   },
   subContainer:{
     borderColor: borderCol,
-    
+    borderWidth: borderWidth,
+    paddingLeft:15,
+    paddingRight:15,
+    justifyContent:'center'
+  },
+  innerSubContainer:{
+    borderColor: borderCol,
+    borderWidth: borderWidth,
+    margin:0,
+    justifyContent:'center'
   },
   header:{
     textAlign: 'center',
     fontSize: fontScale*4,
-    color: "white", 
+    color: "black", 
   },
   text:{
-    color: "white",
+    color: "black",
     textAlign: 'center',
     fontSize: fontScale*3,
   },
   list: {
-    color: "white",
+    color: "black",
     flex: 1,
     marginTop: 20
   },
@@ -260,13 +313,13 @@ const styles = StyleSheet.create({
     margin: 5,
   },
   listItemText: {
-    color: "white",
+    color: "black",
     fontSize: fontScale*4,
     marginBottom: 10
   },
   input:{
     height: 40,
-    margin: 12,
+    margin: 5,
     borderWidth: 2,
     color: 'black',
     borderColor:'black',
@@ -284,6 +337,17 @@ const styles = StyleSheet.create({
     whiteSpace: 'nowrap',
     flexWrap: 'nowrap'
   }, 
+  btads:{
+    height: 100,
+    justifyContent:'center',
+    alignContent:"center"
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+    zIndex: 1
+  },
 });
 
 export default Flex;
